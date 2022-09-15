d0c5af90e21254895a2848f709e9a405cc516668
Devin <duhrich@gmail.com>
2022-09-15 12:46:02 -0600

    pairing session with kevin

--- src/main/java/com/kajabi/gildedrose/GildedRose.java
+++ src/main/java/com/kajabi/gildedrose/GildedRose.java
@@ -1,71 +1,87 @@
 package com.kajabi.gildedrose;
 
 public class GildedRose {
-  private String name;
-  private int daysRemaining;
-  private int quality;
+    public final String aged = "Aged Brie";
+    public final String passes = "Backstage passes to a TAFKAL80ETC concert";
+    public final String legendary = "Legendary Sulfuras, Hand of Ragnaros";
 
-  public GildedRose(String name, int daysRemaining, int quality) {
-    this.name = name;
-    this.daysRemaining = daysRemaining;
-    this.quality = quality;
-  }
+    public final String conjured = "Conjured Mana Cake";
+    private String name;
+    private int daysRemaining;
+    private int quality;
 
-  public String getName() {
-    return name;
-  }
-
-  public int getDaysRemaining() {
-      return daysRemaining;
-  }
-
-  public int getQuality() {
-      return quality;
-  }
-
-  public void tick() {
-    if (this.name != "Aged Brie" && this.name != "Backstage passes to a TAFKAL80ETC concert") {
-      if (this.quality > 0) {
-        if (this.name != "Legendary Sulfuras, Hand of Ragnaros") {
-          this.quality--;
-        }
-      }
-    } else {
-      if (this.quality < 50) {
-        this.quality++;
-        if (this.name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (this.daysRemaining < 11) {
-            if (this.quality < 50) {
-              this.quality++;
-            }
-          }
-          if (this.daysRemaining < 6) {
-            if (this.quality < 50) {
-              this.quality++;
-            }
-          }
-        }
-      }
+    public GildedRose(String name, int daysRemaining, int quality) {
+        this.name = name;
+        this.daysRemaining = daysRemaining;
+        this.quality = quality;
     }
-    if (this.name != "Legendary Sulfuras, Hand of Ragnaros") {
-      this.daysRemaining--;
+
+    public String getName() {
+        return name;
     }
-    if (this.daysRemaining < 0) {
-      if (this.name != "Aged Brie") {
-        if (this.name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (this.quality > 0) {
-            if (this.name != "Legendary Sulfuras, Hand of Ragnaros") {
-              this.quality--;
+
+    public int getDaysRemaining() {
+        return daysRemaining;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void tick() {
+        if (aged.equals(this.name) || passes.equals(this.name)) {
+             if (this.quality < 50) {
+                this.quality++;
+                if (this.name.equals(passes)) {
+                    if (this.daysRemaining < 11) {
+                        increaseQuality();
+                    }
+                    if (this.daysRemaining < 6) {
+                        increaseQuality();
+                    }
+                }
             }
-          }
         } else {
-          this.quality = this.quality - this.quality;
+            safelyDecreaseQuality(this.name);
         }
-      } else {
-        if (this.quality < 50) {
-          this.quality++;
+        decreaseDaysRemaining();
+        if (this.daysRemaining < 0) {
+            if (!this.name.equals(aged)) {
+                if (!this.name.equals(passes)) {
+                    safelyDecreaseQuality(this.name);
+                } else {
+                    this.quality = 0;
+                }
+            } else {
+                increaseQuality();
+            }
         }
-      }
     }
-  }
+
+    private void decreaseDaysRemaining() {
+        if (!this.name.equals(legendary)) {
+            this.daysRemaining--;
+        }
+    }
+
+    private void increaseQuality() {
+        if (this.quality < 50) {
+            this.quality++;
+        }
+    }
+
+    private void safelyDecreaseQuality(String name) {
+        if (this.quality > 0) {
+            if (!this.name.equals(legendary)) {
+                this.quality--;
+                if (this.quality > 0 && shouldDoubleDecrease(name)) {
+                    this.quality--;
+                }
+            }
+        }
+    }
+
+    private boolean shouldDoubleDecrease(String name) {
+        return name.equals(conjured);
+    }
 }
